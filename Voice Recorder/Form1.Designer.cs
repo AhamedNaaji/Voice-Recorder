843b1fac0b46f1d40c71b324caf0a02108f0976d
Ahamed Naaji <naajicreation779@gmail.com>
2024-03-26 01:11:37 +0530

    Updates

--- Voice Recorder/Form1.Designer.cs
+++ Voice Recorder/Form1.Designer.cs
@@ -28,34 +28,108 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnRecord = new System.Windows.Forms.Button();
+            this.BtnSave = new System.Windows.Forms.Button();
+            this.BtnPlay = new System.Windows.Forms.Button();
+            this.lbl_Recorder = new System.Windows.Forms.Label();
+            this.lbl_Timer = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
-            // button1
+            // btnRecord
             // 
-            this.button1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(21, 174);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(154, 45);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Record";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnRecord.BackColor = System.Drawing.Color.Transparent;
+            this.btnRecord.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRecord.Location = new System.Drawing.Point(12, 136);
+            this.btnRecord.Name = "btnRecord";
+            this.btnRecord.Size = new System.Drawing.Size(154, 45);
+            this.btnRecord.TabIndex = 0;
+            this.btnRecord.Text = "Record";
+            this.btnRecord.UseVisualStyleBackColor = false;
+            this.btnRecord.Click += new System.EventHandler(this.btnRecord_Click);
+            // 
+            // BtnSave
+            // 
+            this.BtnSave.BackColor = System.Drawing.Color.Transparent;
+            this.BtnSave.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnSave.Location = new System.Drawing.Point(183, 136);
+            this.BtnSave.Name = "BtnSave";
+            this.BtnSave.Size = new System.Drawing.Size(154, 45);
+            this.BtnSave.TabIndex = 1;
+            this.BtnSave.Text = "Save";
+            this.BtnSave.UseVisualStyleBackColor = false;
+            this.BtnSave.Click += new System.EventHandler(this.BtnSave_Click);
+            // 
+            // BtnPlay
+            // 
+            this.BtnPlay.BackColor = System.Drawing.Color.Transparent;
+            this.BtnPlay.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnPlay.Location = new System.Drawing.Point(357, 136);
+            this.BtnPlay.Name = "BtnPlay";
+            this.BtnPlay.Size = new System.Drawing.Size(154, 45);
+            this.BtnPlay.TabIndex = 2;
+            this.BtnPlay.Text = "Play";
+            this.BtnPlay.UseVisualStyleBackColor = false;
+            this.BtnPlay.Click += new System.EventHandler(this.BtnPlay_Click);
+            // 
+            // lbl_Recorder
+            // 
+            this.lbl_Recorder.AutoSize = true;
+            this.lbl_Recorder.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_Recorder.Font = new System.Drawing.Font("Myanmar Text", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Recorder.ForeColor = System.Drawing.Color.Gold;
+            this.lbl_Recorder.Location = new System.Drawing.Point(12, 25);
+            this.lbl_Recorder.Name = "lbl_Recorder";
+            this.lbl_Recorder.Size = new System.Drawing.Size(185, 41);
+            this.lbl_Recorder.TabIndex = 3;
+            this.lbl_Recorder.Text = "Voice Recorder ";
+            // 
+            // lbl_Timer
+            // 
+            this.lbl_Timer.AutoSize = true;
+            this.lbl_Timer.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_Timer.Font = new System.Drawing.Font("Myanmar Text", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Timer.ForeColor = System.Drawing.Color.Gold;
+            this.lbl_Timer.Location = new System.Drawing.Point(241, 25);
+            this.lbl_Timer.Name = "lbl_Timer";
+            this.lbl_Timer.Size = new System.Drawing.Size(246, 41);
+            this.lbl_Timer.TabIndex = 4;
+            this.lbl_Timer.Text = "Duration : 00 : 00 : 00";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 256);
-            this.Controls.Add(this.button1);
+            this.BackgroundImage = global::Voice_Recorder.Properties.Resources.bg1;
+            this.ClientSize = new System.Drawing.Size(520, 193);
+            this.Controls.Add(this.lbl_Timer);
+            this.Controls.Add(this.lbl_Recorder);
+            this.Controls.Add(this.BtnPlay);
+            this.Controls.Add(this.BtnSave);
+            this.Controls.Add(this.btnRecord);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Voice Recorder";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnRecord;
+        private System.Windows.Forms.Button BtnSave;
+        private System.Windows.Forms.Button BtnPlay;
+        private System.Windows.Forms.Label lbl_Recorder;
+        private System.Windows.Forms.Label lbl_Timer;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- Voice Recorder/Form1.cs
+++ Voice Recorder/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace Voice_Recorder
 {
@@ -15,6 +16,57 @@ namespace Voice_Recorder
         public Form1()
         {
             InitializeComponent();
+        }
+        //Code for importing winm.dynamic - link library methods to access in the program 
+        [DllImport("winmm.dll")]
+        private static extern int mciSendString(string MciComando, string MciRetrno, int MciRetrnoLeng, int Callback);
+        string record = "";
+
+        private void btnRecord_Click(object sender, EventArgs e)
+        {
+            //Code For Recording
+            mciSendString("open new type waveaudio alias Som" , null, 0, 0);
+            mciSendString("record Som", null , 0, 0);
+            lbl_Recorder.Text = "Recording...";
+            lbl_Recorder.ForeColor = Color.Crimson;
+            timer1.Start();
+            Count_Down.Start();
+
+        }
+
+        private void BtnSave_Click(object sender, EventArgs e)
+        {
+            //Code to Save Recording File
+            mciSendString("pause Som", null, 0, 0);
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "wave| *. wav";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                mciSendString("save Som" + sfd.FileName, null, 0, 0);
+                mciSendString("close Som", null, 0, 0);
+            }
+        }
+
+        private void BtnPlay_Click(object sender, EventArgs e)
+        {
+            if (record == "")
+            {
+                OpenFileDialog ofd = new OpenFileDialog();
+                ofd.Filter = "wave| *. wav";
+                if (ofd.ShowDialog() == DialogResult.OK)
+
+                    record = ofd.FileName;
+            }
+            mciSendString("play " + record, null, 0, 0);
+            lbl_Recorder.Text = "Playing .... ";
+        }
+
+        System.Diagnostics.Stopwatch Count_Down = new System.Diagnostics.Stopwatch();
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            TimeSpan elapsed = Count_Down.Elapsed;
+            lbl_Timer.Text = "Duration :" + string.Format("{0:00}:{1:00}:{2:00}", Math.Floor(elapsed.TotalHours),
+            elapsed.Minutes, elapsed.Seconds);
         }
     }
 }
